b921ae38834dde34cced8d77fdc13a64ac828a51
Athziri Rodriguez <athziri.rodriguez@unosquare.com>
2022-02-21 08:01:22 -0600

    Final Pull Request

--- src/test/java/com/unosquare/ApiCore.java
+++ src/test/java/com/unosquare/ApiCore.java
@@ -10,7 +10,7 @@ import io.restassured.specification.RequestSpecification;
 
 public class ApiCore {
 
-	public Response PostLogin(String filePath, String url) {
+	public Response POSTmethod(String filePath, String url) {
 		
 		File jsonDataFile = new File(filePath);
 
@@ -24,8 +24,34 @@ public class ApiCore {
 		
 	}
 	
-	public void logResponse(Response response) {
+	public Response GETmethod(String url) {
+		
 
+		RestAssured.baseURI = "https://reqres.in/api";
+		RequestSpecification httpRequest = RestAssured.given();
+		httpRequest.headers("Content-Type", "application/json");
+		Response response = httpRequest.get(url);
+		
+		return response;
+		
+	}
+	
+	public Response PUTmethod(String filePath, String url) {
+		
+		File jsonDataFile = new File(filePath);
+
+		RestAssured.baseURI = "https://reqres.in/api";
+		RequestSpecification httpRequest = RestAssured.given();
+		httpRequest.headers("Content-Type", "application/json");
+		httpRequest.body(jsonDataFile);
+		Response response = httpRequest.put(url);
+		
+		return response;
+		
+	}
+	
+	public void logResponse(Response response) {
+		
 		Reporter.log("Response body: " + response.body().asString());
 		Reporter.log("Response code: " + response.statusCode());
 		Reporter.log("URL: " + RestAssured.baseURI);
--- src/test/java/com/unosquare/ApplyingEncapsulation.java
+++ src/test/java/com/unosquare/ApplyingEncapsulation.java
@@ -13,12 +13,12 @@ import org.testng.annotations.Test;
 import bsh.ParseException;
 import io.restassured.response.Response;
 
+@Test
 public class ApplyingEncapsulation {
-	@Test
 	public void PostLogin() throws IOException, ParseException {
 
 		ApiCore apiCore = new ApiCore();
-		Response test = apiCore.PostLogin("src\\main\\resources\\login.json", "/login");
+		Response test = apiCore.POSTmethod("src\\main\\resources\\login.json", "/login");
 		
 		Assert.assertEquals(200, test.statusCode());
 		
@@ -26,6 +26,102 @@ public class ApplyingEncapsulation {
 		
 		apiCore.logResponse(test);
 	}
+	
+	public void PostCreate() throws IOException, ParseException {
+
+		ApiCore apiCore = new ApiCore();
+		Response test = apiCore.POSTmethod("src\\main\\resources\\users.json", "/users");
+		
+		Assert.assertEquals(201, test.statusCode());
+		
+		test.then().assertThat().body("$", hasKey("createdAt"));
+		
+		apiCore.logResponse(test);
+	}
+	
+	public void PostRegister() throws IOException, ParseException {
+
+		ApiCore apiCore = new ApiCore();
+		Response test = apiCore.POSTmethod("src\\main\\resources\\login.json", "/register");
+		
+		Assert.assertEquals(200, test.statusCode());
+		
+		test.then().assertThat().body("$", hasKey("token"));
+		
+		apiCore.logResponse(test);
+	}
+	
+	public void PostRegisterUnsuccessfu() throws IOException, ParseException {
+
+		ApiCore apiCore = new ApiCore();
+		Response test = apiCore.POSTmethod("src\\main\\resources\\users.json", "/register");
+		
+		Assert.assertEquals(400, test.statusCode());
+		
+		test.then().assertThat().body("$", hasKey("error"));
+		
+		apiCore.logResponse(test);
+	}
+	
+	public void GetListUsers() throws IOException, ParseException {
+
+		ApiCore apiCore = new ApiCore();
+		Response test = apiCore.GETmethod("/users");
+		
+		Assert.assertEquals(200, test.statusCode());
+
+		test.then().assertThat().body("$", hasKey("total"));
+		test.then().assertThat().body("$", hasKey("data"));
+		
+		apiCore.logResponse(test);
+	}
+	
+	public void GetUser() throws IOException, ParseException {
+
+		ApiCore apiCore = new ApiCore();
+		Response test = apiCore.GETmethod("/users/5");
+		
+		Assert.assertEquals(200, test.statusCode());
+
+		test.then().assertThat().body("$", hasKey("data"));
+		
+		apiCore.logResponse(test);
+	}
+	
+	public void GetUserNotFound() throws IOException, ParseException {
+
+		ApiCore apiCore = new ApiCore();
+		Response test = apiCore.GETmethod("/users/23");
+		
+		Assert.assertEquals(404, test.statusCode());
+		
+		apiCore.logResponse(test);
+	}
+	
+	public void GetUserResource() throws IOException, ParseException {
+
+		ApiCore apiCore = new ApiCore();
+		Response test = apiCore.GETmethod("/unknown/2");
+		
+		Assert.assertEquals(200, test.statusCode());
+
+		test.then().assertThat().body("$", hasKey("data")); 
+		
+		apiCore.logResponse(test);
+	}
+	
+	public void PutUpdate() throws IOException, ParseException {
+
+		ApiCore apiCore = new ApiCore();
+		Response test = apiCore.PUTmethod("src\\main\\resources\\users.json", "/users/2");
+		
+		Assert.assertEquals(200, test.statusCode());
+
+		test.then().assertThat().body("$", hasKey("updatedAt"));
+		
+		apiCore.logResponse(test);
+	}
+		
 
 	@BeforeSuite
 	public void beforeSuite() {
